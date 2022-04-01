a1d0d60d64050c33022e7611226f975ffb1ef37d
bimri <bimri@outlook.com>
2022-04-01 18:11:59 +0300

    Booleans

---
+++ ch2/booleans.go
@@ -0,0 +1,7 @@
+// Booleans
+
+// Variables of bool type can have one of
+// two values: true or false. The zero value for a bool is false:
+
+// var flag bool 			// no value assigned, set to false
+// var isAwesome = true
